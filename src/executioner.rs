d65f245c7ad4508c96181e6c9bef234bd9f1c836
0xEquinox <WilliamJMcdonald@protonmail.com>
2021-08-24 23:34:01 -0700

    fixed spacing

--- src/executioner.rs
+++ src/executioner.rs
@@ -4,10 +4,10 @@ use rand::Rng;
 pub fn password_gen(length: i32, letters: bool, numbers: bool, symbols: bool, all: bool) -> String{
 
         let chars = vec!["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "#", "$", "%", "?", "/", "\\", "*", ",", "."];
-        let mut password:Vec<&str> = Vec::new();
+        let mut password: Vec<&str> = Vec::new();
         let mut password_string = String::new();
         let mut rng = thread_rng();
-        let mut choices:Vec<i32> = Vec::new();
+        let mut choices: Vec<i32> = Vec::new();
 
         for _ in 0..length{
             if all == true{
